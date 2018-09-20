60f6afafc887cdc127d1d352fdf6907b7e533821
Chris Barnard <chris.barnard@arrow.com>
2018-09-19 23:48:52 -0600

    work on form styling

--- src/pages/flowbot.js
+++ src/pages/flowbot.js
@@ -1,12 +1,28 @@
 import React from "react"
 // import { graphql } from "gatsby"
+import { css } from "react-emotion"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
   console.log(data)
+
+  const flex = css`
+    flex: 1 1 auto;
+    width: 100%;
+  `
+
+  const fullWidth = css`
+    width: 100%;
+  `
+
   return (
     <Layout>
-      <h1>Flowbot</h1>
+      <h1
+          className={css`
+            border-bottom: 1px solid;
+          `}
+      >
+        Flowbot</h1>
       <p>Flowbot was designed so that I could stop checking AWW whitewater to see when things started flowing better.. Less High, write more --- verbage. yay</p>
       <form
         name="flowbot"
@@ -14,21 +30,25 @@ export default ({ data }) => {
         data-netlify="true"
         data-netlify-honeypot="field-for-bots"
         action="#"
+        className={css`
+          display: flex;
+          flex-direction: column;
+        `}
       >
         <input name="field-for-bots" type="hidden" />
-        <p>
-          <label>AWW Section URL: <input name="url" placeholder="https://www.americanwhitewater.org/content/River/detail/id/423" type="text" /></label>
+        <p className={flex}>
+          <label>AWW Section URL: <input className={fullWidth} name="url" placeholder="https://www.americanwhitewater.org/content/River/detail/id/423" type="text" /></label>
         </p>
-        <p>
-          <label>Target Flow: <input name="target" placeholder="350" type="text" /></label>
+        <p className={flex}>
+          <label>Target Flow: <input className={fullWidth} name="target" placeholder="350" type="text" /></label>
         </p>
-        <p>
-          <label>Type of target: <select name="type">
+        <p className={flex}>
+          <label>Type of target: <select className={fullWidth} name="type">
             <option value="over">Over</option>
             <option value="under">Under</option>
           </select></label>
         </p>
-        <p>
+        <p className={flex}>
           <button type="submit">Send</button>
         </p>
       </form>
